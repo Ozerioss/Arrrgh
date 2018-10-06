82a149a8983393aa5a60038fecf9b82e34096a4e
Karim <ozerioss@gmail.com>
2018-10-06 22:44:19 +0200

    Merges work for player movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,37 +1,32 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public bool gameEnded = false;
     public float endDelay = 0.5f;
     public int lastScore = 0;
+    string finalScoreTxtName = "FinalScore";
+    public GameObject completeLevelUI;    
 
-    public void GameOver()
-    {
-        if (!gameEnded)
-        {
+    public void CompleteLevel(){        
+        completeLevelUI.transform.Find(finalScoreTxtName).GetComponent<Text>().text = FindObjectOfType<Score>().ScoreValue.text;
+        completeLevelUI.SetActive(true);
+    }
+
+    public void GameOver() {
+        if (!gameEnded) {
             gameEnded = true;
             lastScore = Convert.ToInt32(FindObjectOfType<Score>().ScoreValue.text);
-            Debug.Log("Over : "+lastScore);
+            Debug.Log("Over : " + lastScore);
 
             Invoke("Restart", endDelay);
         }
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void Restart()
-    {
+    void Restart() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,111 @@
 ﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    public Rigidbody rb;
-    public float forwardForce = 500f;
-    public float horizontalForce = 50f;
-    public Vector3 right = new Vector3(0.1f, 0f, 0f);
+    Rigidbody rb;
+    KeyCode moveLeft;
+    KeyCode moveRight;
+    int lane = 2;
+    public float sideSpeed = 3500f;
+    public float forwardSpeed = 5500f;
+    //The purpose of this variable is to lock the players movement until the last one is finished
+    bool controlLocked = false;
+    //This distance represents the step we move by sideways
+    public float distance = 2f;
+    //Stores the X position of the last destination entered
+    float lastDestinationX;
+    Vector3 desiredVelocity;
 
-	// Use this for initialization
-	void Start () {
-        rb.useGravity = true;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        moveLeft = KeyCode.LeftArrow;
+        moveRight = KeyCode.RightArrow;
+        rb = GetComponent<Rigidbody>();
+        lastDestinationX = rb.transform.position.x;
+        desiredVelocity = Vector3.zero;
+    }
+
+    void Update()
+    {
+        //Check if we reached the position wanted
+        float acceptableMaxDistDiff = 1f;
+
+        if (PositionIsReached(acceptableMaxDistDiff))
+        {
+            desiredVelocity = Vector3.zero;
+            controlLocked = false;
+        }
+
+        #region Checking Inputs
+        //Checking for keydown so as to get the input only once per Update
+        //TODO: This has to be adapted to mobile by a "Swap" mecanic
+        //We also check for the lanes so as to stay in the defined lanes
+        //Check if controls are not locks so that they do not interfer with each other unless the last action is finished      
+        if ((Input.GetKeyDown(moveRight)) && (lane < 3) && (!controlLocked))
+        {
+            Debug.Log("Pressing D");
+            //Position we want to reach
+            Vector3 newPos = rb.transform.position + Vector3.right * distance;
+
+            //Changing the players Velocity so that he moves smoothly towards the desired direction.
+            ChangePlayerVelocity(newPos);
+
+            //Since we moved to the right, increment lane number
+            lane++;
+        }
+
+        if ((Input.GetKeyDown(moveLeft)) && (lane > 1) && (!controlLocked))
+        {
+            Debug.Log("Pressing Q");
+            //Position we want to reach
+            Vector3 newPos = rb.transform.position - Vector3.right * distance;
+
+            ChangePlayerVelocity(newPos);
+
+            //Since we moved to the left, decrement lane number
+            lane--;
+        }
+
+        #endregion
+
+    }
 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        //Changing the velocity of the player based on the calculations made before
+        rb.velocity = desiredVelocity * Time.deltaTime + Vector3.forward * forwardSpeed * Time.deltaTime;
+    }
 
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }else if (Input.GetKey("q"))
-        {
-            rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
+    /// <summary>
+    /// Changing the velocity of the player so that he goes in the direction of the specified position.    
+    /// </summary>
+    /// <param name="newPos">Position wanted</param>
+    void ChangePlayerVelocity(Vector3 newPos)
+    {
+        //Lock the control so that future inputs from user do not interfer
+        controlLocked = true;
 
-        //if (Input.GetKey("d"))
-        //{
-        //    if (rb.position.x >= 5 && rb.position.x < 6)
-        //    {
-        //        Debug.Log("In first if.");
-        //        rb.MovePosition(new Vector3(15, rb.position.y, rb.position.z));
-        //    }
-        //    else if (rb.position.x < 5)
-        //    {
-        //        Debug.Log("In second if");
-        //        rb.MovePosition(new Vector3(5, rb.position.y, rb.position.z));
-        //    }
-        //}
+        //Calculate the direction our player should follow based on the position we want to reach.            
+        Vector3 dir = (newPos - rb.transform.position).normalized * sideSpeed;
+
+        //Save the last destination entry wanted so as to check for the distance later.
+        lastDestinationX = newPos.x;
+
+        //Apply the velocity vector
+        desiredVelocity = dir;
+    }
+
+    /// <summary>
+    ///Calculates the distance between the point we need to reach and our current position.
+    ///We check for a threshold, if the X position is under that threshold, we reached the position wanted.
+    ///We can't check for 0 though because if you take the refresh time and the speed into consideration, reaching 0 will rarely happen.
+    ///This way it will reach a certain point X, which will always be the same.
+    /// </summary>
+    /// <param name="threshold"></param>
+    bool PositionIsReached(float threshold)
+    {
+        return Vector3.Distance(Vector3.right * lastDestinationX, Vector3.right * rb.position.x) < threshold;
     }
 }
