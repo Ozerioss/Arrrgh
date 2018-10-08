0ca470266cbe042bf7520c12d1cb9ca07e4702ae
Shyrro <zsahmane@gmail.com>
2018-10-08 21:49:42 +0200

    Test commit cancer

--- Assets/Scripts/UI/LevelComplete.cs
+++ Assets/Scripts/UI/LevelComplete.cs
@@ -7,7 +7,7 @@ public class LevelComplete : MonoBehaviour {
 
 	// Use this for initialization
 	public void LoadNextLevel(){
-		//TODO : add +1 when other scenes will be available
+		//TODO : add +1 when other scenes will be available qsdlkmhsqdf
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 }
