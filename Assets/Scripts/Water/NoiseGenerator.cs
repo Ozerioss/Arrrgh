e72f61a0e998d8b252a9db09f1d702db05232cbd
Karim <ozerioss@gmail.com>
2018-10-08 20:03:50 +0200

    Adds experimentation scene + perlin noise water generation

---
+++ Assets/Scripts/Water/NoiseGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator : MonoBehaviour {
+
+
+    public float power = 3;
+    public float scale = 1;
+    public float timeScale = 1;
+
+
+    private float offsetX;
+    private float offsetY;
+    private MeshFilter mf;
+
+
+	// Use this for initialization
+	void Start () {
+        mf = GetComponent<MeshFilter>();
+        MakeNoise();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MakeNoise();
+        offsetX += Time.deltaTime * timeScale;
+        offsetY += Time.deltaTime * timeScale;
+		
+	}
+
+
+    void MakeNoise()
+    {
+        Vector3[] verticies = mf.mesh.vertices;
+
+        for(int i = 0; i < verticies.Length; i++)
+        {
+            verticies[i].y = CalculateHeight(verticies[i].x, verticies[i].z) * power;
+        }
+
+        mf.mesh.vertices = verticies;
+    }
+
+    float CalculateHeight(float x, float y)
+    {
+        float xCoord = x * scale + offsetX;
+        float yCoord = y * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
---
+++ Assets/Scripts/Water/WaterPlaneGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPlaneGenerator : MonoBehaviour {
+
+    public float size = 1f;
+    public int gridSize = 16;
+
+    private MeshFilter filter;
+
+	void Start () {
+        filter = GetComponent<MeshFilter>();
+        filter.mesh = GenerateMesh();	
+	}
+
+
+    private Mesh GenerateMesh()
+    {
+        Mesh _mesh = new Mesh();
+        var vertices = new List<Vector3>();
+        var normals = new List<Vector3>();
+        var uvs = new List<Vector2>(); // x, z
+
+
+        for(int x = 0; x < gridSize + 1; x++)
+        {
+            for(int y = 0; y < gridSize + 1; y++)
+            {
+                vertices.Add(new Vector3(-size * 0.5f + size * (x / ((float)gridSize)), 0, -size * 0.5f + size * (y / (float)gridSize)));
+                normals.Add(Vector3.up);
+                uvs.Add(new Vector2(x / (float)gridSize, y / (float)gridSize));
+            }
+        }
+
+        var triangles = new List<int>();
+        var verticesCount = gridSize + 1;
+
+        for(int i = 0; i < verticesCount * verticesCount - verticesCount; i++)
+        {
+            if((i + 1) % verticesCount == 0)
+            {
+                continue;
+            }
+
+            // Adds two triangles
+            triangles.AddRange(new List<int>()
+            {
+                i + 1 + verticesCount, i + verticesCount, i,
+                i, i+1, i + verticesCount + 1
+            });
+        }
+
+        _mesh.SetVertices(vertices);
+        _mesh.SetNormals(normals);
+        _mesh.SetUVs(0, uvs);
+        _mesh.SetTriangles(triangles, 0);
+
+        return _mesh;
+    }
+}
