e4ce48f8c3a66c273057a8f5eded4a32adcd1001
Shyrro <zsahmane@gmail.com>
2018-10-08 03:51:55 +0200

    Added shooting mechanism  with bullet prefab

---
+++ Assets/Scripts/Player/Weapon/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+	public Transform firePoint;
+	public GameObject bulletPrefab;
+	public int currentAmmo = 20;
+	public int maxAmmo = 20;
+
+	// Update is called once per frame
+	void Update() {
+		//Fire1 == Space button on computer
+		if (Input.GetButtonDown("Fire1")) {
+			if (currentAmmo > 0 && currentAmmo <= maxAmmo)
+				Shoot();
+			else
+				Debug.Log("Show UI no ammo");
+		}
+	}
+
+	void Shoot() {
+		//We use the bullet prefab rotation, otherwise the bullet is vertical
+		Instantiate(bulletPrefab, firePoint.position, bulletPrefab.transform.rotation);
+		currentAmmo--;		
+	}
+}
