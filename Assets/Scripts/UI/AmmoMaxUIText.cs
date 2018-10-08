8bc57c2f24898f1dfbc069d4c8fe129ad424bce5
Shyrro <zsahmane@gmail.com>
2018-10-08 05:12:27 +0200

    Added Ammo UI + Changed Heart Sprites

---
+++ Assets/Scripts/UI/AmmoMaxUIText.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoMaxUIText : MonoBehaviour {
+
+	public Weapon weapon;
+	Text MaxAmmoValue;
+
+	void Start() {
+		MaxAmmoValue = GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		MaxAmmoValue.text = "/ " + weapon.maxAmmo.ToString();
+	}
+}
