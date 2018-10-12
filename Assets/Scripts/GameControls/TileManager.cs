8e17c91f52fba950d418ca71a840c729dd75e645
Karim <ozerioss@gmail.com>
2018-10-12 22:41:37 +0200

    Adds random obstacle generation

--- Assets/Scripts/GameControls/TileManager.cs
+++ Assets/Scripts/GameControls/TileManager.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour {
 
+    public GameObject[] obstacles;
+    public GameObject[] buffs;
     public GameObject[] tilePrefabs;
-    public int amountOfTiles = 7;
+    public int amountOfTiles = 10;
+    public float xOffset = 3;
+    
 
 
     private Transform playerTransform;
@@ -14,16 +18,20 @@ public class TileManager : MonoBehaviour {
     private int lastPrefabIndex = 0;
     private float safeZone = 15f; //safe zone before deleting tiles
 
+    private float[] xValues = new float[3];
+
     private List<GameObject> activeTiles;
 
 	// Use this for initialization
 	void Start ()
     {
+        xValues = new float[] {-xOffset, (-xOffset + xOffset) / 2, xOffset }; //Position de l'objet sur X
         activeTiles = new List<GameObject>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
         for(int i = 0; i < amountOfTiles; i++)
         {
+            // spawn safe tiles ( no obstacle )
             if (i < 2)
             {
                 SpawnTile(0);
@@ -49,18 +57,23 @@ public class TileManager : MonoBehaviour {
     void SpawnTile(int indexPrefab = -1)
     {
         GameObject go;
-        int newIndex = RandomPrefabIndex(); // get random index
+        GameObject goObstacle;
+        int newIndex = RandomPrefabIndex(obstacles); // get random index
         if(indexPrefab == -1)
         {
-            go = Instantiate(tilePrefabs[newIndex]) as GameObject; //Instantiate random tile from list
+            go = Instantiate(tilePrefabs[0]) as GameObject; //Instantiate random tile from list
+            //goObstacle = Instantiate(obstacles[newIndex]) as GameObject;
+            goObstacle = Instantiate(obstacles[newIndex], new Vector3(GetRandomX(xValues), 0.8f, spawnZ), obstacles[newIndex].transform.rotation);
         }
         else
         {
-            go = Instantiate(tilePrefabs[indexPrefab]);
+            go = Instantiate(tilePrefabs[0]);
+            goObstacle = null;
         }
 
         go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * spawnZ;
+
         spawnZ += tileLength;
         activeTiles.Add(go);
     }
@@ -75,9 +88,9 @@ public class TileManager : MonoBehaviour {
 
 
     // Custom random number generator
-    private int RandomPrefabIndex()
+    private int RandomPrefabIndex(GameObject[] goArray)
     {
-        if(tilePrefabs.Length <= 1)
+        if(goArray.Length <= 1)
         {
             return 0;
         }
@@ -85,9 +98,15 @@ public class TileManager : MonoBehaviour {
         int randomIndex = lastPrefabIndex;
         while(randomIndex == lastPrefabIndex)
         {
-            randomIndex = Random.Range(0, tilePrefabs.Length);
+            randomIndex = Random.Range(0, goArray.Length);
         }
         lastPrefabIndex = randomIndex;
         return randomIndex;
+    }
+
+    private float GetRandomX(float[] xValues)
+    {
+        int randomXIndex = Random.Range(0, xValues.Length);
+        return xValues[randomXIndex];
     }
 }
